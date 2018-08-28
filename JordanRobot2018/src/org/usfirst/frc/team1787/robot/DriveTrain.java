9fc4316f4cfae0af3862b93dac7013e3299b97a4
FRC1787 <orangerobotics@gmail.com>
2018-08-28 16:31:45 -0400

    Figured out errors, Didn't have stuff imported

--- JordanRobot2018/src/org/usfirst/frc/team1787/robot/DriveTrain.java
+++ JordanRobot2018/src/org/usfirst/frc/team1787/robot/DriveTrain.java
@@ -1,5 +1,17 @@
 package org.usfirst.frc.team1787.robot;
 
-public class DriveTrain {
+import com.ctre.phoenix.motorcontrol.can.*;
 
+public class DriveTrain {
+	
+	private final int RIGHT_DRIVE_MASTER_TALON_ID = 9;
+	private final int RIGHT_DRIVE_FOLLOWER_VICTOR_ID = 8;
+	private final int LEFT_DRIVE_MASTER_TALON_ID = 10;
+	private final int LEFT_DRIVE_FOLLOWER_VICTOR_ID = 11;
+	
+	private WPI_TalonSRX rightMaster = new WPI_TalonSRX(RIGHT_DRIVE_MASTER_TALON_ID);
+	private WPI_VictorSPX rightFollower = new WPI_VictorSPX(RIGHT_DRIVE_FOLLOWER_VICTOR_ID);
+	private WPI_TalonSRX leftMaster = new WPI_TalonSRX(LEFT_DRIVE_MASTER_TALON_ID);
+	private WPI_VictorSPX leftFollower = new WPI_VictorSPX(LEFT_DRIVE_FOLLOWER_VICTOR_ID);
+	
 }
