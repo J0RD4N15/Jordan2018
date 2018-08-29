2e2b8b30f1b17d0dbdc939425cf9671f5805063a
Jordan <bawssth15@gmail.com>
2018-08-28 21:18:52 -0400

    Joystick things

--- JordanRobot2018/src/org/usfirst/frc/team1787/robot/OI.java
+++ JordanRobot2018/src/org/usfirst/frc/team1787/robot/OI.java
@@ -3,10 +3,18 @@ package org.usfirst.frc.team1787.robot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.*;
 import edu.wpi.first.wpilibj.command.*;
+import edu.wpi.first.wpilibj.
 
 public class OI {
 	
-	Joystick rightJoy = new Joystick(1);
-	Button button1 = new JoystickButton(rightJoy, 1);
-	Button button2 = new JoystickButton(rightJoy, 2);
+	//Joystick information
+	Joystick RStick = new Joystick(1);
+	Button button1 = new JoystickButton(RStick,1);
+	Button button2 = new JoystickButton(RStick,2);
+	Button button3 = new JoystickButton(RStick,3);
+	Button button4 = new JoystickButton(RStick,4);
+	Button button5 = new JoystickButton(RStick,5);
+	Button button6 = new JoystickButton(RStick,6);
+	Button button7 = new JoystickButton(RStick,7);
+	
 }
